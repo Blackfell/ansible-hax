cddfd987a5e6efe98e5995cd0b7b79ed805f9339
blackfell <github@blackfell.net>
2022-01-02 21:34:54 +0000

    Install script tweaks

--- arch_vm_install.sh
+++ arch_vm_install.sh
@@ -40,11 +40,10 @@ arch-chroot /mnt
 mkinitcpio -P
 
 # The only tools we really need to run ansible
-pacman -Sy sudo git vim python grub --noconfirm
+pacman -Sy sudo git vim python efibootmgr grub --noconfirm
 
 # Install grub - EFI mode
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
- $INSTALL_DISK
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB $INSTALL_DISK
 
 echo "[+] Install complete"
 echo "[!] YOU NEED TO CHANGE THE ROOT PASSWORD"
