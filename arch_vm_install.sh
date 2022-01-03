526d97232394715e24d22f9ffe03effe8471a701
blackfell <github@blackfell.net>
2022-01-03 21:56:43 +0000

    Install script tweaks

--- arch_vm_install.sh
+++ arch_vm_install.sh
@@ -46,7 +46,7 @@ CHRT_CMD="$CHRT_CMD && pacman -Sy sudo git vim python efibootmgr glibc grub --no
 CHRT_CMD="$CHRT_CMD && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB $INSTALL_DISK"
 
 # Now chroot into that bad boy and do STUFF
-arch-chroot /mnt $CHROOT_COMMANDS || echo "[!] Error in chroot."
+arch-chroot /mnt $CHRT_CMD || echo "[!] Error in chroot."
 
 echo "[+] Install complete"
 echo "[!] YOU NEED TO CHANGE THE ROOT PASSWORD"
