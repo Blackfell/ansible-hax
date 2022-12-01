2965f32fc0f1aed7071146d2a6b11aaa06bb0663
blackfell <github@blackfell.net>
2022-12-01 14:37:01 +0000

    updates to compat

--- run.sh
+++ run.sh
@@ -2,13 +2,15 @@
 
 export ANSIBLE_NOCOWS=1
 export REBOOT="--skip-tags reboot"
+export LOCAL="--skip-tags local"
 
-while getopts dc:r option; do
+while getopts dc:rl option; do
   case "${option}" in
     d) export ANSIBLE_COW_SELECTION=random && export ANSIBLE_NOCOWS=0;;
     c) export ANSIBLE_COW_SELECTION=${OPTARG} && export ANSIBLE_NOCOWS=0;;
     r) export REBOOT="";;
+    l) export LOCAL="";;
   esac
 done
 
-ansible-playbook -i inventories/my_inventory/inventory provision.yml -K -b $REBOOT | tee playbook.log
+ansible-playbook -i inventories/my_inventory/inventory provision.yml -K -b $REBOOT $LOCAL | tee playbook.log
